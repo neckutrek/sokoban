42d639be0fece4522ec3539bfc0272dde714cfa2
Marcus Johansson <neckutrek@c-204-222.eduroam.liu.se>
2013-04-30 09:02:11 +0200

    l

--- MaterialManager.cpp
+++ MaterialManager.cpp
@@ -7,3 +7,73 @@
 //
 
 #include "MaterialManager.h"
+
+MaterialManager& MaterialManager::getInstance()
+{
+    static MaterialManager instance;
+    return instance;
+}
+
+MaterialManager::~MaterialManager() {}
+
+void MaterialManager::initialize(GLuint program_reference_id)
+{
+    glUniform1iv(glGetUniformLocation(program_reference_id, "texture_units"),
+                 MAX_LAYERS,
+                 &texture_units_array[0]);
+}
+
+void MaterialManager::setTransparencies(float *transparencies)
+{
+    transparency_array = transparencies;
+}
+
+void MaterialManager::setSpecularities(float *specularities)
+{
+    specularity_array = specularities;
+}
+
+void MaterialManager::setTexureIDs(unsigned int *texture_ids)
+{
+    texture_id_array = texture_ids;
+}
+
+void MaterialManager::setMaterialsCounter(unsigned int count)
+{
+    materials_counter = count;
+    if (materials_counter > MAX_LAYERS)
+        materials_counter = MAX_LAYERS;
+}
+
+int MaterialManager::render(GLuint program_reference_id)
+{
+    glUniform1i(glGetUniformLocation(program_reference_id, "materials_counter"),
+                materials_counter);
+    
+    for (unsigned int i=0; i<materials_counter; ++i) {
+        switch (i) {
+            case 0: glActiveTexture(GL_TEXTURE0); break;
+            case 1: glActiveTexture(GL_TEXTURE1); break;
+            case 2: glActiveTexture(GL_TEXTURE2); break;
+            case 3: glActiveTexture(GL_TEXTURE3); break;
+            case 4: glActiveTexture(GL_TEXTURE4); break;
+            case 5: glActiveTexture(GL_TEXTURE5); break;
+            case 6: glActiveTexture(GL_TEXTURE6); break;
+            case 7: glActiveTexture(GL_TEXTURE7); break;
+            default: break;
+        }
+        glBindTexture(GL_TEXTURE_2D, texture_id_array[i]);
+    }
+    return 0;
+}
+
+MaterialManager::MaterialManager()
+{
+    materials_counter = 0;
+    for (unsigned int i=0; i<MAX_LAYERS; ++i)
+    {
+        texture_units_array[i] = i;
+    }
+}
+MaterialManager::MaterialManager(const MaterialManager&) {}
+MaterialManager& MaterialManager::operator=(const MaterialManager&) {}
--- MaterialManager.h
+++ MaterialManager.h
@@ -9,88 +9,41 @@
 #ifndef __OpenGLGLUTApp__Material__
 #define __OpenGLGLUTApp__Material__
 
-
 #ifdef __APPLE__
-#include <OpenGL/gl3.h>
-#include "MicroGlut.h"
+    #include <OpenGL/gl3.h>
+    #include "MicroGlut.h"
 #endif
+
+#include "GL_utilities.h"
+#include "VectorUtils3Addons.h"
+#include "loadobj.h"
 
 const unsigned int MAX_LAYERS = 8;
 
 class MaterialManager
 {
 public:
-    static MaterialManager& getInstance()
-    {
-        static MaterialManager instance;
-        return instance;
-    }
+    static MaterialManager& getInstance();
     
-    ~MaterialManager() {}
+    ~MaterialManager();
     
-    void initialize(GLuint program_reference_id)
-    {
-        glUniform1iv(glGetUniformLocation(program_reference_id, "texture_units"),
-                     MAX_LAYERS,
-                     &texture_units_array[0]);
-    }
+    void initialize(GLuint program_reference_id);
     
-    void setTransparencies(float *transparencies)
-    {
-        transparency_array = transparencies;
-    }
+    void setTransparencies(float *transparencies);
     
-    void setSpecularities(float *specularities)
-    {
-        specularity_array = specularities;
-    }
+    void setSpecularities(float *specularities);
     
-    void setTexureIDs(unsigned int *texture_ids)
-    {
-        texture_id_array = texture_ids;
-    }
+    void setTexureIDs(unsigned int *texture_ids);
     
-    void setMaterialsCounter(unsigned int count)
-    {
-        materials_counter = count;
-        if (materials_counter > MAX_LAYERS)
-            materials_counter = MAX_LAYERS;
-    }
+    void setMaterialsCounter(unsigned int count);
     
-    int render(GLuint program_reference_id)
-    {
-        glUniform1i(glGetUniformLocation(program_reference_id, "materials_counter"),
-                    materials_counter);
-        
-        for (unsigned int i=0; i<materials_counter; ++i) {
-            switch (i) {
-                case 0: glActiveTexture(GL_TEXTURE0); break;
-                case 1: glActiveTexture(GL_TEXTURE1); break;
-                case 2: glActiveTexture(GL_TEXTURE2); break;
-                case 3: glActiveTexture(GL_TEXTURE3); break;
-                case 4: glActiveTexture(GL_TEXTURE4); break;
-                case 5: glActiveTexture(GL_TEXTURE5); break;
-                case 6: glActiveTexture(GL_TEXTURE6); break;
-                case 7: glActiveTexture(GL_TEXTURE7); break;
-                default: break;
-            }
-            glBindTexture(GL_TEXTURE_2D, texture_id_array[i]);
-        }
-        return 0;
-    }
+    int render(GLuint program_reference_id);
     
     
 private:
-    MaterialManager()
-    {
-        materials_counter = 0;
-        for (unsigned int i=0; i<MAX_LAYERS; ++i)
-        {
-            texture_units_array[i] = i;
-        }
-    }
-    MaterialManager(const MaterialManager&) {}
-    MaterialManager& operator=(const MaterialManager&) {}
+    MaterialManager();
+    MaterialManager(const MaterialManager&);
+    MaterialManager& operator=(const MaterialManager&);
     
     unsigned int texture_units_array[MAX_LAYERS];
     float *transparency_array;
