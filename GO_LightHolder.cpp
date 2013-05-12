3b973e30e45ec19d34676ed354f02d31e7786838
Simons macka <simon.borgenvall@gmail.com>
2013-05-12 14:18:53 +0200

    NU BLIR DET KONFLIKETER

---
+++ GO_LightHolder.cpp
@@ -0,0 +1,9 @@
+//
+//  GO_LightHolder.cpp
+//  OpenGLGLUTApp
+//
+//  Created by Simon Borgenvall on 2013-05-09.
+//
+//
+
+#include "GO_LightHolder.h"
---
+++ GO_LightHolder.h
@@ -0,0 +1,14 @@
+//
+//  GO_LightHolder.h
+//  OpenGLGLUTApp
+//
+//  Created by Simon Borgenvall on 2013-05-09.
+//
+//
+
+#ifndef __OpenGLGLUTApp__GO_LightHolder__
+#define __OpenGLGLUTApp__GO_LightHolder__
+
+#include <iostream>
+
+#endif /* defined(__OpenGLGLUTApp__GO_LightHolder__) */
